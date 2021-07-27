5b13b53db56366f7702d764f7cab4ac42ddf6c2a
carteras <adam.carter@ed.act.edu.au>
2021-07-27 20:55:50 +1000

    mucking about with objects

---
+++ 2021/digital_technologies/digital_applications/learning_briefs/Week 3/code/W3HelloGUI/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace W3HelloGUI
+{
+    
+    public partial class Form1 : Form
+    {
+        List<string> names = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            names.Add(textBox1.Text);
+            listBox1.Items.Add(textBox1.Text);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ 2021/digital_technologies/digital_applications/learning_briefs/Week 3/code/W3Objects/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace W3Objects
+{
+     class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+
+        public string Rego { get; set; }
+
+        private int _year;
+
+        public int Year
+        {
+            get { return _year; }
+            set { _year = value; }
+        }
+
+
+        public int Odometer { set; get; }
+
+
+        public Car(string i)
+        {
+            string[] input = i.Split(':');
+            //VW:Tiguan:1234:123abc
+            this.Make = input[0];
+            this.Model = input[1];
+            this.Year = Convert.ToInt32(input[2]);
+            this.Rego = input[3];
+
+        }
+    }
+
+    class Program
+    {
+        public static List<Car> cars;
+        static void Main(string[] args)
+        {
+            cars = new List<Car>();
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Enter in car details Make:Model:Year:Rego");
+                //VW:Tiguan:1234:123abc
+                string userInput = Console.ReadLine();
+                Car newCar = new Car(userInput);
+                cars.Add(newCar);
+            }
+
+            foreach (Car car in cars)
+            {
+                Debug.WriteLine($"{car.Make}, {car.Model} {car.Year}");
+            }
+
+            //Car newCar = new Car("VW", "Tiguan", "123abc", 2021);
+            //Console.WriteLine($"I own a {newCar.getYear()} {newCar.getMake()} {newCar.getModel()} it has {newCar.getOdometer()}km on the clock.");
+
+        }
+    }
+}
