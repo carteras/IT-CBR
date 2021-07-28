5c56f7682c32ba5cc22b04099a5e763a1dcd8f0b
carteras <adam.carter@ed.act.edu.au>
2021-07-29 08:34:41 +1000

    inheritence

---
+++ 2021/digital_technologies/digital_applications/learning_briefs/Week 4/Inheritence/Inheritence/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Inheritence
+{
+
+    abstract class User {
+        public string Name { get; set; }
+        public User(string name) {
+            Name = name;
+        }
+
+        public abstract string Describe();
+    }
+
+    class Student : User {
+        public int Age { get; set; }
+        public Student(string name, int age) : base(name) {
+            Name = name;
+            Age = age;
+        }
+
+        public override string Describe() {
+            return $"This is {this.Name} and they are {this.Age} years old";
+        }
+    }
+
+    class Teacher : User {
+        public int Salary { get; set; }
+
+        public Teacher(string name, int salary) : base(name) {
+            Name = name;
+            Salary = salary;
+        }
+        public override string Describe() {
+            return $"This is {this.Name} and he earns ${this.Salary}";
+        }
+    }
+    class Program {
+        static List<User> users;
+        public static void Main(string[] args)
+        {
+            users = new List<User>();
+            users.Add(new Student("Ada", 16));
+            users.Add(new Teacher("Adam", 110000));
+
+            foreach (User user in users) {
+                Console.WriteLine(user.Describe());
+            }
+        }
+    }
+}
