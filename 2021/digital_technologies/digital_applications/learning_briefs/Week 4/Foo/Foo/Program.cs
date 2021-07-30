332b5d78d6161a0a0f6243464d9b99e049908050
carteras <adam.carter@ed.act.edu.au>
2021-07-30 14:56:36 +1000

    friday class programming

---
+++ 2021/digital_technologies/digital_applications/learning_briefs/Week 4/Foo/Foo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Foo {
+
+    class Dog {
+        public string Name { get; set; }
+        public string BarkNoise { get; set; }
+        public string Breed { get; set; }
+
+        public Dog(string name, string bark, string breed) {
+            this.Name = name;
+            this.BarkNoise = bark;
+            this.Breed = breed;
+        }
+
+        public string describe() {
+            return $"This dog's name is {this.Name}, they are a {this.Breed}. They go {this.BarkNoise}";
+        }
+
+    }
+    class Program {
+        static List<Dog> dogs;
+        static void Main(string[] args) {
+            dogs = new List<Dog>();
+            dogs.Add(new Dog("Sam", "woof", "Greyhound"));
+            dogs.Add(new Dog("Max", "yip", "Poodle"));
+
+            foreach (Dog dog in dogs) {
+                Console.WriteLine(dog.describe());
+            }
+        }
+    }
+}
