04c321a17a7b9604ef8331867533885bef0b8ba6
carteras <adam.carter@ed.act.edu.au>
2021-08-04 08:12:48 +1000

    Networking week 4b

---
+++ 2021/digital_technologies/digital_applications/learning_briefs/Week 4/Test/Test/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Test {
+
+    abstract class Foo{
+        public string Thing { get; set; }
+
+        public abstract string describe();
+    }
+
+    class Bar: Foo {
+        public Bar() {
+            Thing = "What?";
+        }
+
+        public override string describe() {
+            return $"{Thing} is in Bar";
+        }
+    }
+
+    class Boo: Foo {
+        public Boo() {
+            Thing = "What what?";
+        }
+        public override string describe() {
+            return $"I am not bar, I am boo {Thing}";
+        }
+    }
+    class Program {
+        static void Main(string[] args) {
+            var foos = new List<Foo>();
+            foos.Add(new Bar());
+            foos.Add(new Boo());
+
+            foreach (Foo f in foos) {
+                Console.WriteLine(f.describe());
+            }
+        }
+    }
+}
