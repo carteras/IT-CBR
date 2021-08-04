541d8c929c706ceb67a39121885aa50a1db0bb25
carteras <adam.carter@ed.act.edu.au>
2021-08-04 12:54:05 +1000

    Digital Applications Week 4: Inheritance (in class)
    A copy of the work done in class. This work reflects one of the practice questions.

---
+++ 2021/digital_technologies/digital_applications/learning_briefs/Week 4/DogPark/DogPark/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DogPark {
+
+    abstract class Dog {
+        public string Name { get; set; }
+        public string BarkSound { get; set; }
+
+        public string Bark() {
+            return $"{Name} barks at you! {BarkSound} {BarkSound}";
+        }
+    }
+
+
+    class Greyhound : Dog {
+
+        public Greyhound(string name) {
+            Name = name;
+            BarkSound = "yep";
+        }
+ 
+        
+    }
+
+    class Poodle : Dog {
+
+        public Poodle(string name) {
+            Name = name;
+            BarkSound = "woof";
+        }
+     
+    }
+
+    class DogPark {
+        List<Dog> dogs = new List<Dog>();
+        
+        public void AddDog(Dog d) {
+            dogs.Add(d);
+        }
+
+        public void run() {
+            foreach (Dog d in dogs) {
+                Console.WriteLine(d.Bark());
+            }
+        }
+    }
+    class Program {
+        static void Main(string[] args) {
+            DogPark dp = new DogPark();
+            dp.AddDog(new Greyhound("Sam"));
+
+            dp.run();
+        }
+    }
+}
