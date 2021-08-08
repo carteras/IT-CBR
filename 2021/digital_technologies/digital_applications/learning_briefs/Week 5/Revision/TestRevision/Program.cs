da46400f07f37eabd9344f132b3d0afb26c79393
carteras <adam.carter@ed.act.edu.au>
2021-08-08 19:36:07 +1000

    Create Program.cs

---
+++ 2021/digital_technologies/digital_applications/learning_briefs/Week 5/Revision/TestRevision/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TestRevision {
+    class Program {
+        enum Level {
+            Awesome = 1,
+            Cool = 2,
+        }
+        static void Main(string[] args) {
+            var foo = Level.Cool;
+
+            Console.WriteLine((int)foo) ;
+
+        }
+    }
+}
