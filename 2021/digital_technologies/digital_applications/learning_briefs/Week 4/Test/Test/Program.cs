021877fcd2cab3f5bc26e6d6d7b9dd615a88c967
carteras <adam.carter@ed.act.edu.au>
2021-08-09 08:27:38 +1000

    Update Program.cs

--- 2021/digital_technologies/digital_applications/learning_briefs/Week 4/Test/Test/Program.cs
+++ 2021/digital_technologies/digital_applications/learning_briefs/Week 4/Test/Test/Program.cs
@@ -2,39 +2,23 @@
 using System.Collections.Generic;
 namespace Test {
 
-    abstract class Foo{
-        public string Thing { get; set; }
 
-        public abstract string describe();
-    }
-
-    class Bar: Foo {
-        public Bar() {
-            Thing = "What?";
-        }
-
-        public override string describe() {
-            return $"{Thing} is in Bar";
-        }
-    }
-
-    class Boo: Foo {
-        public Boo() {
-            Thing = "What what?";
-        }
-        public override string describe() {
-            return $"I am not bar, I am boo {Thing}";
-        }
-    }
     class Program {
-        static void Main(string[] args) {
-            var foos = new List<Foo>();
-            foos.Add(new Bar());
-            foos.Add(new Boo());
 
-            foreach (Foo f in foos) {
-                Console.WriteLine(f.describe());
+        public static string foo(List<string> f) {
+            string foo = "";
+            foreach (String s in f) {
+                foo += s;
             }
+            return foo;
+        }
+        static void Main(string[] args) {
+
+            var tt = new List<string>();
+            tt.Add("foo");
+            tt.Add("bar");
+            Console.WriteLine(foo(tt));
+            
         }
     }
 }
