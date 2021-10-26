a096f20d8aa528710aaf8dd3fce58a3e02d7b8f6
carteras <adam.carter@ed.act.edu.au>
2021-10-27 09:47:45 +1100

    I think my gitignore broke?

--- 2021/digital_technologies/digital_applications/learning_briefs/Week 4/Test/Test/Program.cs
+++ 2021/digital_technologies/digital_applications/learning_briefs/Week 4/Test/Test/Program.cs
@@ -2,23 +2,62 @@
 using System.Collections.Generic;
 namespace Test {
 
+    abstract class Message {
+        public string Payload { get; set; }
+        public string Address { get; set; }
+        public string Sender { get; set; }
+
+        public Message(string address, string payload) {
+            Payload = payload;
+            Address = address;
+        }
+
+        public abstract string SendMessage();
+    }
+
+    class SMS : Message {
+
+        public SMS(string phoneNumber, string payload) : base(phoneNumber, payload){
+            Sender = "Foo SMS Service!";
+        }
+
+        public override string SendMessage() {
+            return $"{{\"phoneTo\" : {Address}, \"packet\" :{Payload} , \"senderPhone\" : {Sender}}}";
+        }
+    }
+
+    class Email : Message {
+        public Email(string address, string payload) : base(address, payload) {
+            Sender = "admin@acme.com";
+        }
+
+        public override string SendMessage() {
+            return $"{{ 'sender':{Sender}, 'receiver':{Address} 'msg':{Payload}   }}";
+        }
+    }
+
+    class MsgEvent {
+        public Message Msg { get; set; }
+
+        public void ChangeMessage(Message m) {
+            Msg = m;
+        }
+        public string SendMessage() {
+            return Msg.SendMessage();
+        }
+    }
 
     class Program {
-
-        public static string foo(List<string> f) {
-            string foo = "";
-            foreach (String s in f) {
-                foo += s;
-            }
-            return foo;
-        }
         static void Main(string[] args) {
 
-            var tt = new List<string>();
-            tt.Add("foo");
-            tt.Add("bar");
-            Console.WriteLine(foo(tt));
-            
+            MsgEvent msgevt = new MsgEvent();
+            msgevt.ChangeMessage(new SMS("12345678", "fjoifwejioewf"));
+            Console.WriteLine(msgevt.SendMessage());
+            msgevt.ChangeMessage(new Email("admin@google.com", "fo fo fo fo fo fo fo fo "));
+            Console.WriteLine(msgevt.SendMessage());
+
+
+
         }
     }
 }
---
+++ Tools/classroom_documentation_tool/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+doc = DocxTemplate(output_path / "foo_out.docx")
+# doc.render(context)
+# doc.save(output_path/"foo_out.docx")
