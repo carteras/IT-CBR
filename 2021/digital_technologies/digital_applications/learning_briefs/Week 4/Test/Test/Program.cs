ad6e7989470bd644d444d7646047a1d76c9cbe5d
carteras <adam.carter@ed.act.edu.au>
2022-02-04 10:04:46 +1100

    old stuff, disregard.

--- 2021/digital_technologies/digital_applications/learning_briefs/Week 4/Test/Test/Program.cs
+++ 2021/digital_technologies/digital_applications/learning_briefs/Week 4/Test/Test/Program.cs
@@ -2,62 +2,59 @@
 using System.Collections.Generic;
 namespace Test {
 
-    abstract class Message {
-        public string Payload { get; set; }
-        public string Address { get; set; }
-        public string Sender { get; set; }
+    public abstract class Door {
 
-        public Message(string address, string payload) {
-            Payload = payload;
-            Address = address;
-        }
+        public char ASCII { get; set; }
 
-        public abstract string SendMessage();
-    }
+        public abstract void Open();
 
-    class SMS : Message {
-
-        public SMS(string phoneNumber, string payload) : base(phoneNumber, payload){
-            Sender = "Foo SMS Service!";
-        }
-
-        public override string SendMessage() {
-            return $"{{\"phoneTo\" : {Address}, \"packet\" :{Payload} , \"senderPhone\" : {Sender}}}";
+        public virtual char Draw() {
+            return ASCII;
         }
     }
 
-    class Email : Message {
-        public Email(string address, string payload) : base(address, payload) {
-            Sender = "admin@acme.com";
+    public class ClosedState : Door {
+        public override void Open() {
+            ASCII = '+';
         }
 
-        public override string SendMessage() {
-            return $"{{ 'sender':{Sender}, 'receiver':{Address} 'msg':{Payload}   }}";
+    }
+    public class OpenedState : Door {
+        public override void Open() {
+            ASCII = '=';
         }
     }
 
-    class MsgEvent {
-        public Message Msg { get; set; }
-
-        public void ChangeMessage(Message m) {
-            Msg = m;
-        }
-        public string SendMessage() {
-            return Msg.SendMessage();
+    public class LockedState : Door {
+        public override void Open() {
+            ASCII = '±';
         }
     }
 
-    class Program {
+    public class SchoolDoor : Door {
+        public Door D {get; set;}
+
+        public SchoolDoor() {
+            D = new OpenedState();
+        }
+
+        public override void Open() {
+             D.Open();
+        }
+
+        public override char Draw() {
+            return D.Draw();
+        }
+
+    }
+    public class Program { 
         static void Main(string[] args) {
-
-            MsgEvent msgevt = new MsgEvent();
-            msgevt.ChangeMessage(new SMS("12345678", "fjoifwejioewf"));
-            Console.WriteLine(msgevt.SendMessage());
-            msgevt.ChangeMessage(new Email("admin@google.com", "fo fo fo fo fo fo fo fo "));
-            Console.WriteLine(msgevt.SendMessage());
-
-
-
+            SchoolDoor A308 = new SchoolDoor();
+            Console.WriteLine(A308.Draw());
+            A308.D = new ClosedState();
+            Console.WriteLine(A308.Draw());
+            A308.D = new LockedState();
+            Console.WriteLine(A308.Draw());
         }
     }
 }
