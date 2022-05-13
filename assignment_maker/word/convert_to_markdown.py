c98c7022ec6c7e989294587ce781b3dfe0d94969
carteras <adam.carter@ed.act.edu.au>
2022-05-13 10:53:10 +1000

    Updated to convert word documents in subdirectories in assigments

--- assignment_maker/word/convert_to_markdown.py
+++ assignment_maker/word/convert_to_markdown.py
@@ -1,6 +1,5 @@
 from lib2to3.pytree import convert
 from pathlib import Path
-from inspect import currentframe, getframeinfo
 from os import system
 
 def find_dirs(location):
@@ -10,16 +9,13 @@ def find_files(location):
     return [file for file in location.iterdir() if file.is_file()]
 
 def convert_to_markdown():
-    filename = getframeinfo(currentframe()).filename
-    here_dir = Path(filename).resolve().parent
-    output_dir = here_dir / "output"
-
-    files = find_files(output_dir)
-
-    for file in files:
-        md_path = file.parent / f"{file.stem}"
-        print(f"MAKING: {md_path}.md")
-        system(f"pandoc {file} -f docx -t markdown-simple_tables-multiline_tables-grid_tables -o {md_path}.md")
+    output_dir = Path.cwd() / "assignments"
+    for dir in find_dirs(output_dir):
+        for file in find_files(dir):
+            md_path = dir/file.parent / f"{file.stem}"
+            print(f"FINDING {file}")
+            print(f"MAKING: {md_path}.md")
+            system(f'pandoc "{file}" -f docx -t markdown-simple_tables-multiline_tables-grid_tables -o "{md_path}.md"')
 
 if __name__ == "__main__":
     convert_to_markdown()
