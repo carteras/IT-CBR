ed2c6bf16e130534055c406a6b5b73e26ae0f598
carteras <adam.carter@ed.act.edu.au>
2022-06-23 19:32:15 +1000

    ADMIN: updating generic report writer with dynamic file loading.

--- Tools/generic_report_writer/generic_report_writer.py
+++ Tools/generic_report_writer/generic_report_writer.py
@@ -2,19 +2,44 @@ from pathlib import Path
 from inspect import currentframe, getframeinfo
 
 filename = getframeinfo(currentframe()).filename
+docs = Path.cwd() / '.foo' / 'reports'
 here = Path(filename).resolve().parent
 
-reports = {
-    'a' : "I'm afraid for the calendar. Its days are numbered. (A)",
-    'b' : "what’s black and yellow and flies at 30,000 feet? (B)ees on an airplane.",
-    'c' : "did you hear about the pirate who had trouble with the alphabet? He always got lost at C!",
-    'd' : "which bear is the most condescending? A pan-(D)uh!",
-    'e' : "why are sheep such bad drivers? They always make illegal (E)we turns!",
-    'v' : "(V)oilà! In (v)iew, a humble (v)aude(v)illian (v)eteran, cast (v)icariously as both (v)ictim and (v)illain by the (v)icissitudes of Fate. This (v)isage, no mere (v)eneer of (v)anity, is a (v)estige of the (v)ox populi, now (v)acant, (v)anished. Howe(v)er, this (v)alorous (v)isitation of a bygone (v)exation stands (v)i(v)ified, and has (v)owed to (v)anquish these (v)enal and (v)irulent (v)ermin (v)anguarding (v)ice and (v)ouchsafing the (v)iolently (v)icious and (v)oracious (v)iolation of (v)olition! The only (v)erdict is (v)engeance; a (v)endetta held as a (v)oti(v)e, not in (v)ain, for the (v)alue and (v)eracity of such shall one day (v)indicate the (v)igilant and the (v)irtuous. (V)erily, this (v)ichyssoise of (v)erbiage (v)eers most (v)erbose, so let me simply add that it's my (v)ery good honor to meet you and you may call me (V).",
-}
+reports = {}
 
-def get_report(name, grade):
+def get_generic_descriptor(grade: str) -> str: 
+    '''
+    Dynamically loads generic descriptors for games. 
+
+    Searches for and finds a file with the appropriate grade. 
+
+    params
+    : grade - the grade to get a descriptor
+
+    returns
+    : str - a string describing the grade
+    '''
+    with open(docs/grade) as fd: return fd.readlines()
+
+def get_report(name: str, grade: str) -> str:
+    '''
+    Generates a generic report based on grade. 
+
+    params
+    : name - the students name
+    : grade - the grade of the student. Used as key
+    
+    returns
+    : string - a bespoke string based off of given grade. 
+    '''
     return f"{name}, {reports[grade]}"
+
+reports["a"] = get_generic_descriptor('A')
+reports["b"] = get_generic_descriptor('B')
+reports["c"] = get_generic_descriptor('C')
+reports["d"] = get_generic_descriptor('D')
+reports["e"] = get_generic_descriptor('E')
+reports["v"] = get_generic_descriptor('V')
 
 with open(here/"names") as fd:
     for line in fd.readlines():
