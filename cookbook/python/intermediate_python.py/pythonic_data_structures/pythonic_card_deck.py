7e98325e1d47e9b5ee1179aabf083deb6dfa9117
carteras <adam.carter@ed.act.edu.au>
2022-08-01 06:37:48 +1000

    ADMIN: future me problem - intermediate python recipe

---
+++ cookbook/python/intermediate_python.py/pythonic_data_structures/pythonic_card_deck.py
@@ -0,0 +1,39 @@
+from collections import namedtuple
+Card = namedtuple('Card', ['rank', 'suit'])
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split(" ")
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits
+                                        for rank in self.ranks]
+    
+    def __len__(self):
+        return len(self._cards)
+    
+    def __getitem__(self, position):
+        return self._cards[position]
+
+deck = FrenchDeck()
+print(len(deck))
+print(deck[1])
+print(deck[-1])
+print(deck[:5])
+
+from random import choice
+print(choice(deck))
+
+for card in deck:
+    print(card)
+
+print(Card("Q", "hearts") in deck) 
+
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
+
+for card in sorted(deck, key=spades_high):
+    print(card)
